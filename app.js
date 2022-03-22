754a89ca733700de3e126de7937ea43fab813865
zachary webb <zachsavn@gmail.com>
2022-03-22 16:08:56 -0400

    filled out exercises

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
 
 // Exercise 1
-
+ const firstName = "Zachary";
+ let lastName = "Webb";
+ var age = 27;
 // Exercise 2
-
+let templateFullName = `${firstName} ${lastName}`;
+let fullName = firstName + " " + lastName;
+console.log(fullName)
+console.log(templateFullName)
 // Exercise 3
+let myStory;
+let home ="Circleville"
+let pasttime ="reading"
+myStory = `My name is ${fullName}. I reside in ${home}. I enjoy ${pasttime}`
+
+console.log(myStory)
