efd1518ca021bd13f51075ca6e2011e20aa2611b
zachary webb <zachsavn@gmail.com>
2022-03-22 16:09:33 -0400

    js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
 // Exercise 1
  const firstName = "Zachary";
  let lastName = "Webb";
